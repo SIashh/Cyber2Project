42895b5697be36501bcba5adfbc5f376a7b71e46
DJ-Slash <djouinot@yahoo.fr>
2020-01-21 18:27:23 +0100

    Automatic redirection to login page if a user requests home page

--- Cyber2Project/urls.py
+++ Cyber2Project/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path()
+    path('', login_required(TemplateView.as_view(template_name='home.html')), name='home')
 ]
