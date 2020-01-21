686a42051994fa337b5f54856704590ffc85803f
DJ-Slash <djouinot@yahoo.fr>
2020-01-21 18:21:52 +0100

    Basic login functionalities (login/logout)

--- Cyber2Project/settings.py
+++ Cyber2Project/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect the user to the home if logged in
+LOGIN_REDIRECT_URL = 'home'
+
+# Redirect the user to the home if loggin out
+LOGOUT_REDIRECT_URL = 'home'
