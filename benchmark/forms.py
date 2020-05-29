576a41fef11686d24a49d00a74fba4a664eac34e
DJ-Slash <djouinot@yahoo.fr>
2020-05-29 18:42:08 +0200

    Added blue.json in castle/static. Modified views.py (benchmark_blue). Modified template staff.html

--- benchmark/forms.py
+++ benchmark/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ChoiceField, Form, Select
+from django.forms import ChoiceField, Form, Select, RadioSelect
 from django.urls import reverse
 from crispy_forms.bootstrap import FormActions, InlineField, InlineRadios
 from crispy_forms.helper import FormHelper
@@ -17,10 +17,11 @@ CRITERIA_RED = {
 }
 
 CHOICES = (
-    (1, "Pas du tout important"),
-    (2, "Pas important"),
-    (3, "Important"),
-    (4, "Très important")
+    (1, "Niveau 1 (moins bonne notation)"),
+    (2, "Niveau 2"),
+    (3, "Niveau 3"),
+    (4, "Niveau 4"),
+    (5, "Niveau 5 (meilleure notation)")
 )
 
 class CustomerBlueForm(Form):
@@ -31,3 +32,13 @@ class CustomerBlueForm(Form):
 
 class CustomerRedForm(Form):
         prix = ChoiceField(widget=Select, choices=CHOICES, initial=CRITERIA_RED['prix'])
+
+class StaffBlueForm(Form):
+        capacité_d_analyse = ChoiceField(widget=Select(attrs={'class': 'mt-1'}), choices=CHOICES)
+        détection = ChoiceField(widget=Select(attrs={'class': 'mt-1'}), choices=CHOICES)
+        complexité_d_analyse = ChoiceField(widget=Select(attrs={'class': 'mt-1'}), choices=CHOICES)
+        export_des_résultats = ChoiceField(widget=Select(attrs={'class': 'mt-1'}), choices=CHOICES)
+        création_de_règles = ChoiceField(widget=Select(attrs={'class': 'mt-1'}), choices=CHOICES)
+        export_de_fichier_suspect = ChoiceField(widget=Select(attrs={'class': 'mt-1'}), choices=CHOICES)
+        prévention = ChoiceField(widget=Select(attrs={'class': 'mt-1'}), choices=CHOICES)
+
--- benchmark/views.py
+++ benchmark/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect, reverse
 from django.utils.translation import ugettext as _
 
-from .forms import CRITERIA_BLUE, CRITERIA_RED, CustomerBlueForm, CustomerRedForm
+from .forms import CRITERIA_BLUE, CRITERIA_RED, CustomerBlueForm, CustomerRedForm, StaffBlueForm
 
 
 def is_member_staff(user):
@@ -18,8 +18,12 @@ def is_member_customers(user):
 @login_required
 @user_passes_test(is_member_staff)
 def staff_blue(request):
+    form1 = StaffBlueForm()
+    form2 = StaffBlueForm()
     return render(request, 'benchmark/staff.html', {
-        "team": "blue"
+        "team": "défensives",
+        "form1" : form1,
+        "form2" : form2
     })
 
 
@@ -55,21 +59,35 @@ def customer_red(request):
 @user_passes_test(is_member_customers)
 def benchmark_blue(request):
     if request.method == 'POST':
-        form = CustomerBlueForm(data=request.POST)
+        form = StaffBlueForm(data=request.POST)
+        form2 = StaffBlueForm(data=request.POST)
+
         criteria = {}
-        if form.is_valid():
-            for criterion in form.cleaned_data.items():
+
+        if form2.is_valid():
+            for criterion in form2.cleaned_data.items():
+                print(criterion)
                 name, weight = criterion
                 criteria[name] = weight
+        if form.is_valid():
+            for criterion in form.cleaned_data.items():
+                print(criterion)
+                name, weight = criterion
+                criteria[name] = weight
+                first_tool = "Nessus" #Exemples, à modifier
+                second_tool = "Qualys" #Exemples, à modifier
             # generate PDF document
             # return PDF document
-            return HttpResponse(1337)
+            return render(request, 'benchmark/report.html', {
+                "team": "blue",
+                "first_tool": first_tool,
+                "second_tool": second_tool
+            })
         else:
             messages.error(request, _('Formulaire invalide.'))
     else:
         return redirect(reverse('customer_blue'))
     pass
-
 
 @login_required
 @user_passes_test(is_member_customers)
